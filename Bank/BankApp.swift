1fa717fda258a10fcdacb6f3b13394e493f15f47
Ronald Munoz <ronald1195@icloud.com>
2024-03-15 14:59:00 -0600

    Initial Commit

---
+++ Bank/BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankApp.swift
+//  Bank
+//
+//  Created by Ronald Muñoz on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
