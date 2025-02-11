2cf3885cda84a04626aab90512a1201de7a92dfc
Ronald Munoz <ronald1195@icloud.com>
2025-02-10 23:37:16 -0700

    Fix game summary view

--- Bank/GameSummaryView.swift
+++ Bank/GameSummaryView.swift
@@ -1,33 +1,34 @@
-////
-////  GameSummaryView.swift
-////  Bank
-////
-////  Created by Ronald Muñoz on 1/24/25.
-////
 //
-//import SwiftUI
+//  GameSummaryView.swift
+//  Bank
 //
-//struct GameSummaryView: View  {
-//    @Binding var players: [Player]
-//    
-//    var body: some View {
-//        NavigationStack{
-//            List {
-//                ForEach(players.sorted { $0.points > $1.points }) { player in
-//                    HStack {
-//                        Text(player.name)
-//                        Spacer()
-//                        Text("\(player.points) pts")
-//                            .foregroundColor(.gray)
-//                    }
-//                }
-//            }
-//            .navigationBarTitle("Game Summary")
-//        }
-//        
-//    }
-//}
+//  Created by Ronald Muñoz on 1/24/25.
 //
-//#Preview {
-//    GameSummaryView(players: .constant(Player.final_game_samples))
-//}
+
+import SwiftUI
+
+struct GameSummaryView: View  {
+    @EnvironmentObject var gameManager: GameManager
+
+    var body: some View {
+        NavigationStack{
+            List {
+                ForEach(gameManager.players.sorted { $0.points > $1.points }) { player in
+                    HStack {
+                        Text(player.name)
+                        Spacer()
+                        Text("\(player.points) pts")
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .navigationBarTitle("Game Summary")
+        }
+        
+    }
+}
+
+#Preview {
+    GameSummaryView()
+        .environmentObject(GameManager(players: Player.final_game_samples))
+}
